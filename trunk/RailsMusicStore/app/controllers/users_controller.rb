733e9cb2376b930430a30ff1a345b73e637d3654
brodock <brodock@8dc75776-5140-0410-9be6-f394e1e5df3f>
2007-12-03 18:31:40 +0000

    user registration improoved main website improoved

--- trunk/RailsMusicStore/app/controllers/users_controller.rb
+++ trunk/RailsMusicStore/app/controllers/users_controller.rb
@@ -3,9 +3,11 @@ class UsersController < ApplicationController
     redirect_to "/"
   end
 
+  attr_accessor :passwd
+     
   # GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
   verify :method => :post, :only => [ :destroy, :create, :update ],
-         :redirect_to => { :action => :list }
+    :redirect_to => { :action => :list }
 
   def register
     @user = User.new
@@ -13,30 +15,32 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(params[:user])
+    @user.credits=0
+    @user.total_spent=0
     if @user.save
       flash[:notice] = 'User was successfully created.'
-      redirect_to :action => 'list'
+      redirect_to :controller => 'main', :action => 'index'
     else
-      render :action => 'new'
+      render :action => 'register'
     end
   end
 
-#  def edit
-#    @user = User.find(params[:id])
-#  end
+  #  def edit
+  #    @user = User.find(params[:id])
+  #  end
 
-#  def update
-#    @user = User.find(params[:id])
-#    if @user.update_attributes(params[:user])
-#      flash[:notice] = 'User was successfully updated.'
-#      redirect_to :action => 'show', :id => @user
-#    else
-#      render :action => 'edit'
-#    end
-#  end
-#
-#  def destroy
-#    User.find(params[:id]).destroy
-#    redirect_to :action => 'list'
-#  end
+  #  def update
+  #    @user = User.find(params[:id])
+  #    if @user.update_attributes(params[:user])
+  #      flash[:notice] = 'User was successfully updated.'
+  #      redirect_to :action => 'show', :id => @user
+  #    else
+  #      render :action => 'edit'
+  #    end
+  #  end
+  #
+  #  def destroy
+  #    User.find(params[:id]).destroy
+  #    redirect_to :action => 'list'
+  #  end
 end
