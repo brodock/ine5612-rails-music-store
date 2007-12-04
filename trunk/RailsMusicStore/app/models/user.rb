dd8d320c40dca1c99062215badc2440df469fc83
brodock <brodock@8dc75776-5140-0410-9be6-f394e1e5df3f>
2007-12-04 02:21:03 +0000

    Login fully operational

--- trunk/RailsMusicStore/app/models/user.rb
+++ trunk/RailsMusicStore/app/models/user.rb
@@ -5,4 +5,8 @@ class User < ActiveRecord::Base
   validates_presence_of :name,:passwd,:email,:cpf
   validates_numericality_of :cpf
   validates_uniqueness_of :name
+  
+  def self.authenticate(name, password)
+    find(:first, :conditions => { :name => name, :passwd => password })
+  end
 end
