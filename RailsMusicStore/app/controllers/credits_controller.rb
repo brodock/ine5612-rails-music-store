efd6425cc64e0a34ad6f80111f7e564ba4cd8108
brodock <brodock@8dc75776-5140-0410-9be6-f394e1e5df3f>
2007-12-06 22:44:50 +0000

    Buying Credits now works gracefully

--- RailsMusicStore/app/controllers/credits_controller.rb
+++ RailsMusicStore/app/controllers/credits_controller.rb
@@ -1,5 +1,16 @@
 class CreditsController < ApplicationController
   def buy
     
+    
+    if request.post?
+      User.transaction @current_user do
+        @current_user.credits = @current_user.credits + params[:credits].to_i
+        @current_user.save
+      end
+      flash[:notice] = "Compra de créditos realizada com sucesso!"
+      redirect_to :controller => 'main'
+    else
+    render :layout => 'ajax'
+    end
   end
 end
